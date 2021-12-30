6d4122789a9d497ed3ef1e6cfc63c7759da98d7f
Shenwei <692949389@QQ.com>
2021-12-30 17:55:51 +0800

    Create ex4.pl

---
+++ 2/ex4.pl
@@ -0,0 +1,7 @@
+#! usr/bin/perl
+print "Please enter a number\n";
+$number_1 = <STDIN>;
+print "Please enter another number\n";
+$number_2 = <STDIN>;
+$result = "$number_1" * "$number_2";
+print "The result is $result\n"
