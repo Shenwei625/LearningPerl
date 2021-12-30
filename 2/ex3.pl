8975e26f574127c68776b4084226c95b6fc3f6c0
Shenwei <692949389@QQ.com>
2021-12-30 17:55:49 +0800

    Create ex3.pl

---
+++ 2/ex3.pl
@@ -0,0 +1,9 @@
+#! usr/bin/perl
+$circle_r = <STDIN>;
+$circle_pai = 3.141592654;
+$circle_length = "$circle_r" * 2 * "$circle_pai";
+if ( $circle_r < 0 ) {
+    print "The result is 0\n";
+}else{
+    print "The result is $circle_length\n";
+}
