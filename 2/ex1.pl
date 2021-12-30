bd101f6f56ca509e0e5e4626560671b778b74772
Shenwei <692949389@QQ.com>
2021-12-30 17:24:02 +0800

    Update ex1.pl

--- 2/ex1.pl
+++ 2/ex1.pl
@@ -1,5 +1,6 @@
 #！ usr/bin/perl
-$circle_r = 12.5
-$circle_pai = 3.141592654
-$circle_length = "$circle_r * 2 * $circle_pai "
-print $circle_length
+$circle_r = 12.5;
+$circle_pai = 3.141592654;
+$circle_length = "$circle_r" * 2 * "$circle_pai" ;
+print "$circle_length\n"
+
