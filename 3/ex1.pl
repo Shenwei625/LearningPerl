f926038b07bc5bfbced959ee937397433abf21ec
Shenwei <692949389@QQ.com>
2021-12-31 14:36:43 +0800

    Update ex1.pl

--- 3/ex1.pl
+++ 3/ex1.pl
@@ -1,1 +1,7 @@
 #! usr/bin/perl
+print "Enter some lines,with the end of Ctrl+D\n";
+@arry_1 = <STDIN>;
+@arry_2 = reverse @arry_1;
+print "The result is:\n@arry_2\n";
+chomp(@arry_1);
+print "@arry_1\n"
